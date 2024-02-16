6b6660b9cd3aa2cb9e0e6831568494a136a066b9
Ziv Mor <0xZivMor@gmail.com>
2024-02-16 02:26:14 +0200

    added imports, renamed method

--- stitt.py
+++ stitt.py
@@ -1,33 +1,42 @@
 import torch
-import torch_geometric
+import torch.nn as nn
+import torch_geometric as pyg
 import scipy.sparse as sp
 
-def compute_normalized_laplacian_eig(graph):
-  """
-  Compute the eigenvalues and eigenvectors of the normalized Laplacian matrix of a graph.
+from typing import Tuple, Optional
 
-  Args:
-    graph (torch_geometric.data.Data): The input graph.
 
-  Returns:
-    tuple: A tuple containing the sorted eigenvalues and eigenvectors of the normalized Laplacian matrix.
-  """
-  
-  laplacian = torch_geometric.utils.get_laplacian(graph.edge_index, normalization="sym")
-  row, col = laplacian[0]
-  sparse_lap = sp.coo_matrix((laplacian[1], (row, col)), (graph.num_nodes, graph.num_nodes))
-  
-  # Eigendecomposition on sparse matrices is not complete
-  lap = torch.tensor(sparse_lap.toarray())
+def get_normalized_laplacian_eig(graph: pyg.data.Data) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Compute the eigenvalues and eigenvectors of the normalized Laplacian matrix of a graph.
 
-  # Compute the eigendecomposition of the Laplacian
-  eigenvalues, eigenvectors = torch.linalg.eigh(lap)
-  eigenvalues = torch.real(eigenvalues)
-  eigenvectors = torch.real(eigenvectors)
-  
-  # S
-  sorted_indices = torch.argsort(eigenvalues, descending=True)
-  eigenvalues = eigenvalues[sorted_indices]
-  eigenvectors = eigenvectors[:, sorted_indices]
+    Args:
+      graph (pyg.data.Data): The input graph.
 
-  return eigenvalues, eigenvectors
+    Returns:
+      tuple: A tuple containing the sorted eigenvalues and eigenvectors of the normalized Laplacian matrix.
+    """
+
+    laplacian = pyg.utils.get_laplacian(
+        graph.edge_index, normalization="sym"
+    )
+    row, col = laplacian[0]
+    sparse_lap = sp.coo_matrix(
+        (laplacian[1], (row, col)), (graph.num_nodes, graph.num_nodes)
+    )
+
+    # Eigendecomposition on sparse matrices is not complete
+    lap = torch.tensor(sparse_lap.toarray())
+
+    # Compute the eigendecomposition of the Laplacian
+    eigenvalues, eigenvectors = torch.linalg.eigh(lap)
+    eigenvalues = torch.real(eigenvalues)
+    eigenvectors = torch.real(eigenvectors)
+
+    # S
+    sorted_indices = torch.argsort(eigenvalues, descending=True)
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:, sorted_indices]
+
+    return eigenvalues, eigenvectors
+
