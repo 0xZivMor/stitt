960fcfe4667d34475dffa856c859c95bb74acab5
Ziv Mor <0xZivMor@gmail.com>
2024-02-18 10:49:43 +0200

    normalization of the laplacian is now an option

--- stitt.py
+++ stitt.py
@@ -6,19 +6,20 @@ import scipy.sparse as sp
 from typing import Tuple, Optional
 
 
-def get_normalized_laplacian_eig(graph: pyg.data.Data) -> Tuple[torch.Tensor, torch.Tensor]:
+def get_laplacian_eig(graph: pyg.data.Data, normalization:Optional[str]=None) -> Tuple[torch.Tensor, torch.Tensor]:
     """
     Compute the eigenvalues and eigenvectors of the normalized Laplacian matrix of a graph.
 
     Args:
       graph (pyg.data.Data): The input graph.
+      normalization (Optional[str]): The type of normalization to be applied to the Laplacian matrix. Default is None.
 
     Returns:
       tuple: A tuple containing the sorted eigenvalues and eigenvectors of the normalized Laplacian matrix.
     """
 
     laplacian = pyg.utils.get_laplacian(
-        graph.edge_index, normalization="sym"
+        graph.edge_index, normalization=normalization
     )
     row, col = laplacian[0]
     sparse_lap = sp.coo_matrix(
@@ -33,7 +34,7 @@ def get_normalized_laplacian_eig(graph: pyg.data.Data) -> Tuple[torch.Tensor, to
     eigenvalues = torch.real(eigenvalues)
     eigenvectors = torch.real(eigenvectors)
 
-    # S
+    # Sort the eigenvalues and eigenvectors in descending order
     sorted_indices = torch.argsort(eigenvalues, descending=True)
     eigenvalues = eigenvalues[sorted_indices]
     eigenvectors = eigenvectors[:, sorted_indices]
