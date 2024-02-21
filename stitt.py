0443a19c99611a20dedd7a83f277113ed88a4dea
Ziv Mor <0xZivMor@gmail.com>
2024-02-21 19:38:43 +0200

    remove batch support, bugfixes

--- stitt.py
+++ stitt.py
@@ -110,16 +110,23 @@ class TransformerBlock(nn.Module):
       ffn (FFN): The feed-forward network.
     """
 
-    def __init__(self, d_input: int, attn_dim: int, mlp_dim: int, num_heads: int):
+    def __init__(self, d_input: int, attn_dim: int, mlp_dim: int, num_heads: int, return_attention: Optional[bool]=True):
         super().__init__()
         self.d_input = d_input
         self.attn_dim = attn_dim
         self.mlp_dim = mlp_dim
         self.num_heads = num_heads
+        self.return_attention = return_attention
 
-        self.rmsnorm1 = RMSNorm(attn_dim)
-        self.rmsnorm2 = RMSNorm(mlp_dim)
-        self.attention = nn.MultiheadAttention(attn_dim, num_heads, batch_first=True)
+        self.rmsnorm1 = RMSNorm(attn_dim * num_heads)
+        self.rmsnorm2 = RMSNorm(d_input)
+        self.attention = nn.MultiheadAttention(
+            attn_dim * num_heads,
+            num_heads,
+            batch_first=True,
+            kdim=d_input,
+            vdim=d_input,
+        )
         self.ffn = FFN(d_input, mlp_dim)
 
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -134,7 +141,7 @@ class TransformerBlock(nn.Module):
           attn_scores (torch.Tensor): The attention weights of each of the attention heads. Shape: (batch_size, num_heads, seq_length, seq_length)
         """
 
-        attended_values, attention_weights = self.self_attention(x, x, x)
+        attended_values, attention_weights = self.attention(x, x, x)
         normalized1 = self.rmsnorm1(attended_values)
         residual1 = x + normalized1
 
@@ -166,7 +173,7 @@ class Stitt(nn.Module):
       features_embed (AtomEncoder): Linear layer for embedding node features.
       geometric_embed (nn.Linear): Linear layer for embedding geometric features.
       transformer_blocks (nn.ModuleList): List of transformer blocks.
-      
+
     Returns:
       torch.Tensor: Output tensor after passing through the Stitt module.
 
@@ -183,55 +190,67 @@ class Stitt(nn.Module):
         d_ffn: int,
         num_heads: int,
         n_layers: int,
+        device: torch.device,
         node_features: Optional[int] = 0,
+        *args,
+        **kwargs,
     ):
-        super(Stitt, self).__init__()
+        super(Stitt, self).__init__(*args, **kwargs)
 
         self.n_features = node_features
         self.d_input = d_input
+        self._device = device
 
         self.features_embed = AtomEncoder(emb_dim=d_input)
         self.geometric_embed = nn.Linear(d_input, d_input)
 
         self.transformer_blocks = nn.ModuleList(
             [
-                TransformerBlock(d_input, d_attn, d_ffn, num_heads)
+                TransformerBlock(d_input, d_attn, d_ffn, num_heads, False)
                 for _ in range(n_layers)
             ]
         )
-        
 
-    
-    def forward(self, features_batch: torch.Tensor, eigvects_batch: torch.Tensor) -> torch.Tensor:
+    def forward(self, features: torch.Tensor, eigvects: torch.Tensor) -> torch.Tensor:
+        # expects features shape [V, features]
+        # expects eigvects shape [V, V]
         # batches should have the same size
-        if features_batch.size(0) != eigvects_batch.size(0):
-          raise ValueError("Must have the same number of graphs and eigenvectors")
-        
-        embedded_features = self.features_embed(features_batch)
+        if features.size(0) != eigvects.size(0):
+            raise ValueError("Must have the same number of graphs and eigenvectors")
 
-        # Pad eigvects_batch with zeros up to the maximum graph size
-        padded = torch.zeros((eigvects_batch.size(0),
-                  eigvects_batch.size(1),
-                  self.d_input, self.d_input))
-        padded[:, :, :eigvects_batch.size(2), :eigvects_batch.size(3)] = eigvects_batch
+        embedded_features = self.features_embed(features)
+
+        # Pad eigvects with zeros up to the maximum graph size
+        padded = torch.zeros(eigvects.size(0), self.d_input, device=self._device)
+        padded[:, : eigvects.size(1)] = eigvects
         embedded_geometrics = self.geometric_embed(padded.detach())
 
         x = embedded_features + embedded_geometrics
+        x = x.unsqueeze(0)  # add batch dim
 
         for block in self.transformer_blocks:
-          x = block()
+            x = block(x)
         return x
 
 
 class StittGraphClassifier(nn.Module):
-  
-  def __init__(self, d_input: int, d_attn: int,  n_heads: int, d_ffn: int, n_layers: int, n_classes: int,
-               *args, **kwargs):
-      super().__init__(*args, **kwargs)
-      self.stitt = Stitt(d_input, d_attn, d_ffn, n_heads, n_layers)
-      self.classifier = nn.Linear(d_input, n_classes)
-      
-  def forward(self, features_batch: torch.Tensor, eigvects_batch: torch.Tensor) -> torch.Tensor:
-    
-    x = self.stitt(features_batch, eigvects_batch)
-    return self.classifier(x) 
+
+    def __init__(
+        self,
+        d_input: int,
+        d_attn: int,
+        n_heads: int,
+        d_ffn: int,
+        n_layers: int,
+        n_classes: int,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.stitt = Stitt(d_input, d_attn, d_ffn, n_heads, n_layers)
+        self.classifier = nn.Linear(d_input, n_classes)
+
+    def forward(self, features: torch.Tensor, eigvects: torch.Tensor) -> torch.Tensor:
+
+        x = self.stitt(features, eigvects)
+        return self.classifier(x)
